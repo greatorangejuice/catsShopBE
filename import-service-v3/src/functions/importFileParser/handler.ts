cf5c9f874b7d4981bb3c7bb7e145ea63fbb6e05a
Pavel Snigirev <psnigirev@elinext.com>
2021-08-20 18:45:03 +0300

    feat: add copyObject and deleteObject after parse

--- import-service-v3/src/functions/importFileParser/handler.ts
+++ import-service-v3/src/functions/importFileParser/handler.ts
@@ -9,10 +9,6 @@ const BUCKET = 'rs-uploaded'
 const importFileParser = async () => {
     const s3 = new AWS.S3({region: 'eu-west-1'});
     let results = [];
-    const getObjectParams = {
-        Bucket: BUCKET,
-        Key: 'uploadedFiles/',
-    }
 
     const listObjectsParams = {
         Bucket: BUCKET,
@@ -20,44 +16,30 @@ const importFileParser = async () => {
     }
 
     try {
-        const s3response = await s3.listObjectsV2(listObjectsParams).createReadStream()
+        const s3response = await s3.listObjectsV2(listObjectsParams).promise()
         results = s3response.Contents.filter(item => item.Size)
-        console.log(results)
-        console.log('STREAM')
-        // Тянем адрес файлика и парсим.
-        s3.getObject(getObjectParams)
+        const getObjectParams = {
+            Bucket: BUCKET,
+            Key: '',
+        }
+        if (results[0] && results[0].Size) {
+            getObjectParams.Key = results[0]['Key'];
+        }
+        await s3.getObject(getObjectParams)
             .createReadStream()
             .pipe(csv())
             .on('data', (data) => results.push(data))
             .on('end', () => {
                 console.log('RESULTS IN STREAM', results);
-            });
+            })
+        await s3.copyObject({Bucket: BUCKET, CopySource: BUCKET + '/' + getObjectParams.Key, Key: getObjectParams.Key.replace('uploadedFiles', 'parsed')}).promise()
+        await s3.deleteObject(({Bucket: BUCKET, Key: getObjectParams.Key})).promise()
+
     } catch (e) {
         console.error(e.message)
     }
 
-    // console.log('GetObject')
-    // const response = await s3.getObject(getObjectParams).promise();
-    // const fileContent = response.Body.toString('utf-8');
-    // console.log(fileContent)
-    // s3.getObject(getObjectParams, (err, data) => {
-    //     console.log('IN GETOBJECT FUNC')
-    //     if (err) console.log(err, err.stack)
-    //     else console.log('DATA IN GET OBJECT: ', data)
-    // });
-
-    // console.log('Get ListObjectsV2')
-    // const filesList = await s3.listObjectsV2(listObjectsParams).promise()
-    // console.log('FILELIST CONTENTS: ', filesList.Contents)
-
-
-    // fs.createReadStream('uploadedFiles/testFile.csv')
-    //     .pipe(csvParser())
-    //     .on('data', (data) => results.push(data))
-    //     .on('end', () => {
-    //         console.log('RESULTS IN STREAM', results);
-    //     });
-    return formatJSONResponse({message: 'All works'}, 200)
+    return formatJSONResponse({message: 'All works'}, 202)
 }
 
 
