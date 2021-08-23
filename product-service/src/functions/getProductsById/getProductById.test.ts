b5a57730f9b2e44b9f87592047a77e1e63ea6231
Pavel Snigirev <43197939+greatorangejuice@users.noreply.github.com>
2021-08-23 15:24:31 +0300

    feat: add tests for getProductsById

--- product-service/src/functions/getProductsById/getProductById.test.ts
+++ product-service/src/functions/getProductsById/getProductById.test.ts
@@ -1,18 +1,25 @@
-const sum = (a = 0, b = 0) => {
-    return a + b
-}
+import {getCatById} from "./handler";
 
-import {getCatById} from "@functions/index";
-
-test('basic', () => {
-    expect(sum()).toBe(0);
-});
-test('basic again', () => {
-    expect(sum(1, 2)).toBe(3);
+test('getProductsById should return response with empty cats array and message with Cat not found', async () => {
+    const event = {pathParameters: {id: 7}};
+    const {body} = await getCatById(event);
+    const result = JSON.parse(body);
+    expect(result).toStrictEqual({message: "Cat not found", cats: []});
 });
 
+test('getProductsById should return response with only one product array', async () => {
+    const event = {pathParameters: {id: 0}};
+    const {body} = await getCatById(event);
+    const result = JSON.parse(body);
 
-test('get cat with id 1', () => {
-    // @ts-ignore
-    expect(getCatById().toBe(Object as any))
-})
+    expect(result).toStrictEqual({
+        cats: [{
+            age: 1,
+            breed: 'Abyssinian Cat',
+            id: 0,
+            imgLink: 'https://www.thesprucepets.com/thmb/7p0TopOHEHX3aQsdYzRdidbS0Lo=/2121x1414/filters:fill(auto,1)/GettyImages-165827729-efc11c02690f457a81ef6ccbfa8eb34d.jpg',
+            name: 'Tom',
+            price: 100
+        }]
+    });
+});
