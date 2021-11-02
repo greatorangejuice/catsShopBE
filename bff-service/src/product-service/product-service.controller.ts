c4b2d1b9d0ffcb3c108fccdc4146d0e1b209f43c
Pavel Snigirev <psnigirev@elinext.com>
2021-11-02 23:34:46 +0300

    feat: add cache for products service

--- bff-service/src/product-service/product-service.controller.ts
+++ bff-service/src/product-service/product-service.controller.ts
@@ -1,12 +1,14 @@
-import {Controller, Get, Post, Request} from '@nestjs/common';
+import {CacheInterceptor, Controller, Get, Post, Request, UseInterceptors} from '@nestjs/common';
 import {ProductProcessorService} from "./product-processor.service";
 import {createThirdApiUrl} from "../utils/sliceUrl";
+import {HttpCacheInterceptor} from "../common/http-cache.interceptor";
 
 @Controller('product')
+@UseInterceptors(HttpCacheInterceptor)
 export class ProductServiceController {
     constructor(private readonly productService: ProductProcessorService) {
     }
-
+    
     @Get()
     getAllProducts(@Request() request) {
         const url = createThirdApiUrl(request.url, 'product')
--- bff-service/src/product-service/product-service.module.ts
+++ bff-service/src/product-service/product-service.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {CacheModule, Module} from '@nestjs/common';
 import { ProductProcessorService } from './product-processor.service';
 import { ProductServiceController } from './product-service.controller';
 import {HttpModule} from "@nestjs/axios";
@@ -8,6 +8,6 @@ import {HttpModule} from "@nestjs/axios";
   controllers: [ProductServiceController],
   imports: [HttpModule.register({
     // baseURL: 'https://d8r3r3d0h3.execute-api.eu-west-1.amazonaws.com/dev/products'
-  })]
+  }), CacheModule.register({ttl: 120})]
 })
 export class ProductServiceModule {}
